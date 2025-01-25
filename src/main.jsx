a7a60e28946dac3b4105c64fbab8860b1f3990ad
Ankit <ankitkerketta03517@gmail.com>
2025-01-25 21:05:57 +0530

    big

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import ContextProvider from '.context.jsx'
+import ContextProvider from './context/Context'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<ContextProvider>
