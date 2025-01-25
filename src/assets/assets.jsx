65a67ad1d105ed97f61c94119593390ca28e8766
Ankit <ankitkerketta03517@gmail.com>
2025-01-25 20:42:10 +0530

    first commit

---
+++ src/assets/assets.jsx
@@ -0,0 +1,33 @@
+import bulb from './bulb_icon.png'
+import code from './code_icon.png'
+import compass from './compass_icon.png'
+import gallery from './gallery_icon.png'
+import gemini from './gemini_icon.png'
+import history from './history_icon.png'
+import message from './message_icon.png'
+import microphone from './mic_icon.png'
+import plus from './plus_icon.png'
+import user from './user_icon.png'
+import send from './send_icon.png'
+import setting from './setting_icon.png'
+import youtube from './youtube_icon.png'
+import menu from './menu_icon.png'
+import question from './question_icon.png'
+
+export const assets ={
+    bulb,
+    code,
+    compass,
+    gallery,
+    gemini,
+    history,
+    message,
+    microphone,
+    plus,
+    user,
+    send,
+    setting,
+    youtube,
+    menu,
+    question,
+}
