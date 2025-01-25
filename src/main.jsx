1c45d2ce4b68fd319505308ceeeecca4f676b56a
Ankit <ankitkerketta03517@gmail.com>
2025-01-25 21:14:27 +0530

    minor

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import ContextProvider from './context/Context'
+import ContextProvider from './context/context.jsx'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<ContextProvider>
